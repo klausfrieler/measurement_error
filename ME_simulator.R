f78117815bc0a88bef4bb266a53fa6c7931186fa
klausfrieler <kf@omniversum.de>
2025-01-28 21:34:19 +0100

    Removed end batch message.

--- ME_simulator.R
+++ ME_simulator.R
@@ -228,8 +228,6 @@ ME_simulator <- R6::R6Class("ME_simulator",
                                             #browser()
                                             ret
                                           })
-                                          messagef("************* END BATCH ***********")
-                                          
                                         })
                                       })
                                     })  %>%
@@ -238,6 +236,7 @@ ME_simulator <- R6::R6Class("ME_simulator",
                                              measurement_error_raw = me_raw)
                                   })
                                   if(self$scenario == 3){
+                                    browser()
                                     self$results <- self$results %>% 
                                       mutate(measurement_error = factor(measurement_error, 
                                                                         levels = scenario3_me_levels))

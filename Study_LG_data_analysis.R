9a1a8723ab25e63944d9c9fe3ffcd3f536451ca5
Klaus Frieler <klaus.frieler@ad.aesthetics.mpg.de>
2025-01-30 16:08:55 +0100

    Fixed MI correction for sc3n5000.

--- Study_LG_data_analysis.R
+++ Study_LG_data_analysis.R
@@ -1,15 +1,22 @@
 ### Study 2: Anaysis empirical data from LongGold
+library(tidyverse)
 
 LG_dat <- read.csv("~/Documents/Conferences/2025_ICMPC/select_vars.csv",colClasses=c("p_id"="character"))
+#LG_dat <- read.csv("c:/Users/klaus.frieler/Downloads/select_vars.csv",colClasses=c("p_id"="character"))
 
 ### Pre-process data:
 ### use only cases where BAT, MIQ, GMS.MT are not missing
 LG_dat <- LG_dat[!is.na(LG_dat$BAT.score) & !is.na(LG_dat$MIQ.score) & !is.na(LG_dat$GMS.musical_training), ]
 
 ### remove all cases without a valid ID (p_id not starting with "0")
-
+LG_dat <- LG_dat %>% filter(substr(p_id, 1, 1) == "0")
+  
 ### use each p_id only once, selecting teh observation from teh latest test wave or highest age
-
+LG_dat <- LG_dat %>% 
+  group_by(p_id) %>% 
+  mutate(last = test_wave == max(test_wave)) %>% 
+  ungroup() %>% 
+  filter(last) 
 ### demographics
 
 mean(LG_dat$age)
@@ -18,11 +25,11 @@ table(LG_dat$gender) / sum(table(LG_dat$gender))
 
 ### Data analysis with and without correction; only MI correction for now
 require(TSI)
-
+source("c:/Users/klaus.frieler/MPIAE/projects/measurement_error/my_TSI.R")
 m_naive <- lm(BAT.score ~ MIQ.score + GMS.musical_training,data=LG_dat)
 
-mice.dfs = TSI(
-  LG_dat %>% select(-c(p_id)),
+mice.dfs = my_TSI(
+  LG_dat %>% select(BAT.score, BAT.error, MIQ.score, MIQ.error, GMS.musical_training),
   os_names = c("BAT.score", "MIQ.score"),
   se_names = c("BAT.error", "MIQ.error"),
   metrics = "z",
